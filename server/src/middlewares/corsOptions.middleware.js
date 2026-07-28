9c979a44458905436d9ce7fe1fa5bff27bbba728
Sieghartm4 <sieghartm4@gmail.com>
2026-07-28 10:28:46 +0800

    [UPDATE] CORS

--- server/src/middlewares/corsOptions.middleware.js
+++ server/src/middlewares/corsOptions.middleware.js
@@ -14,7 +14,6 @@ const corsOptions = {
     : [
         buildUrl(process.env._CLIENT_FULL_URL, process.env._CLIENT_URL, process.env._CLIENT_PORT),
         buildUrl(process.env._SERVER_FULL_URL, process.env._SERVER_URL, process.env._SERVER_PORT),
-        `http://localhost:${process.env._SERVER_PORT}`,
         'https://theanxietynurse.com',
         'https://api.theanxietynurse.com',
       ],
